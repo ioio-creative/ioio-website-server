732f57c8a2ad6410f04202511cc167dacbb3cb28
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-06-14 00:22:46 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ioio-website-server
-This server is based on the very good tutorial "Deploy React and Express to Heroku" by Dave Ceddia. https://daveceddia.com/deploy-react-express-app-heroku/
+This Node JS Express server is based on the very good tutorial "Deploy React and Express to Heroku" by Dave Ceddia.
+
+https://daveceddia.com/deploy-react-express-app-heroku/
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+
+const sourceFileBuildDirectory = 'client/build';
+const defaultSourceDocument = 'index.html';
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, sourceFileBuildDirectory)));
+
+// Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+  // const count = 5;
+
+  // // Generate some passwords
+  // const passwords = Array.from(Array(count).keys()).map(i =>
+  //   generatePassword(12, false)
+  // )
+
+  // // Return them as json
+  // res.json(passwords);
+
+  // console.log(`Sent ${count} passwords`);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, sourceFileBuildDirectory + defaultSourceDocument));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`IOIO website server listening on ${port}`);
